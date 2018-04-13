102d3a6628b426507966875eb0a478d77d8ee7f2
Maxx Bartko <12576535+maxxbartko@users.noreply.github.com>
2018-04-12 19:28:29 -0700

    Webpack and babel implemented. Working on getting React to render on index.html

---
+++ client/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+React.DOM.render(
+  <span role="img" aria-label="Skull and Crossbones">
+  ☠️　ＥＴＳＹＣＵＴＩＯＮＥＲ　☠️
+  </span>,
+  document.getElementById('app'),
+);
+
+module.hot.accept();
