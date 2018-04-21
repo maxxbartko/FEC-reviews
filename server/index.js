a9204ab64acb8ad865d5bbeceb5016e88976fb5d
Maxx Bartko <12576535+maxxbartko@users.noreply.github.com>
2018-04-21 14:40:09 -0400

    GET and POST request now both work. Minimums for API call complete.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const mysql = require('promise-mysql');
+const bodyParser = require('body-parser');
 
-const app = express();
 let connection;
-// let shopId;
+const app = express();
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/listings/:listingId/reviews/', (req, res) => {
-  console.log('G E T request');
+  mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'etsycutioner',
+  }).then((conn) => {
+    // capture connection
+    connection = conn;
+    // retrieve all listings for shop
+    return connection.query('SELECT shop FROM listings WHERE listings.id = ?', req.params.listingId);
+  }).then((data) => {
+    // pick shop id out of data packet
+    const { shop } = data[0];
+    // return all reviews for shop
+    return connection.query('SELECT DISTINCT reviews.id, reviews.person, reviews.shop, reviews.listing, reviews.body, reviews.date, reviews.stars FROM reviews, listings WHERE listings.shop = ? AND listings.shop = reviews.shop', shop);
+  }).then(data => res.send(data))
+    .catch(err => res.send`⚠️ Error responding to GET request: ${err}`);
+});
+
+app.post('/listings/:listingId/reviews/', (req, res) => {
+  const {
+    person, shop, listing, body, stars,
+  } = req.body;
+  const date = (new Date()).toISOString();
+  const review = [
+    person, shop, listing, body, date.split('T').join(' ').slice(0, 19), stars,
+  ];
+  let reviewsCount;
+  let totalStars;
+  let avgStarsPerCent;
 
   mysql.createConnection({
     host: '127.0.0.1',
@@ -15,48 +45,27 @@ app.get('/listings/:listingId/reviews/', (req, res) => {
     password: '',
     database: 'etsycutioner',
   }).then((conn) => {
+    // capture connection
     connection = conn;
-    return connection.query(`SELECT shop FROM listings WHERE listings.id = ${req.params.listingId}`);
-  }).then((packet) => {
-    const { shop } = packet[0];
-    return connection.query(`SELECT DISTINCT reviews.id, reviews.person, reviews.shop, reviews.listing, reviews.body, reviews.date, reviews.stars FROM reviews, listings WHERE listings.shop = ${shop} AND listings.shop = reviews.shop`);
-  }).then(data => res.send(data))
-    .catch(err => console.log`⚠️ Error responding to GET request: ${err}`);
+    // insert review to database
+    return connection.query('INSERT INTO reviews (person, shop, listing, body, date, stars) VALUES (?)', [review]);
+  }).then(() => connection.query('SELECT reviews_count, avg_stars_per_cent FROM shops WHERE id = ?', shop))
+    .then((data) => {
+      // retrieve reviews_count and avg_stars_per_cent data from shops
+      reviewsCount = data[0].reviews_count;
+      avgStarsPerCent = data[0].avg_stars_per_cent;
+      totalStars = reviewsCount * avgStarsPerCent;
+      // add one new review to shop's count
+      reviewsCount++;
+      // add new stars to total stars
+      totalStars += req.body.stars;
+      // calculate new average star percentage for shop
+      avgStarsPerCent = Number((totalStars / reviewsCount).toFixed(0));
+    })
+    // update shop review information
+    .then(() => connection.query('UPDATE shops SET reviews_count = ?, avg_stars_per_cent = ? WHERE id = ?', [reviewsCount, avgStarsPerCent, shop]))
+    .then(() => res.send`✅ Review submitted. Thank you for your feedback!`)
+    .catch(err => res.send`⚠️ Error responding to POST request: ${err}`);
 });
 
-// app.post('listings/:listingId/reviews/', (req, res) => {
-//   console.log('P O S T request');
-
-//   const {
-//     person, shop, listing, body, stars,
-//   } = req.body;
-//   const date = (new Date()).toISOString();
-//   const review = [
-//     person, shop, listing, body, date.split('T').join(' ').slice(0, 19), stars,
-//   ];
-
-//   let reviewsData;
-
-//   mysql.createConnection({
-//     host: '127.0.0.1',
-//     user: 'root',
-//     password: '',
-//     database: 'etsycutioner',
-//   }).then((conn) => {
-//     connection = conn;
-//     return connection.query('INSERT INTO reviews VALUES (person, shop, listing, body, date, stars) ?', [review]);
-//   }).then(() => connection.query(`SELECT reviews_count, avg_stars_per_cent FROM shops WHERE shops.id = ${shop}`))
-//     .then((packet) => {
-//       console.log(packet);
-//       const { reviewsCount, avgStarsPerCent } = packet[0];
-//     })
-//     .then(data => res.send(data))
-//     .catch(err => console.log`⚠️ Error responding to GET request: ${err}`);
-// });
-
 app.listen(6300, () => console.log`👌`);
-
-// post request
-// first: insert into db
-// then: retrieve shop reviews length and average
-// then: overwrite shop reviews length and average
